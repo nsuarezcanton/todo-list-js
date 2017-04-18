6272a9bda8c21c2f806ad64a382e6839d8edcc75
Nico Suarez-Canton Trueba <nsuarez.canton@gmail.com>
2017-04-18 13:04:51 -0400

    added styles; need to finish editing logic

--- phase_1/js/todo_list.js
+++ phase_1/js/todo_list.js
@@ -7,6 +7,12 @@ for (index = 0; index < taskList.length; index++) {
 	removeButton.className = "remove_button";
 	removeButton.appendChild(txt);
 	taskList[index].appendChild(removeButton);
+
+	var editSpan = document.createElement("SPAN");
+	var editTxt = document.createTextNode("Edit");
+	editSpan.className = "edit_button";
+	editSpan.appendChild(editTxt);
+	taskList[index].appendChild(editSpan);
 }
 
 // Click on a "done" button to hide the current list item
@@ -15,6 +21,14 @@ for (var index = 0; index < removeButton.length; index++) {
 	removeButton[index].onclick = function() {
 		var div = this.parentElement;
 		div.style.display = "none";
+	}
+}
+
+var editButton = document.getElementsByClassName("edit_button");
+for (var index = 0; index < editButton.length; index++) {
+	editButton[index].onclick = function() {
+		var div = this.parentElement;
+		div.setAttribute("contentEditable", true);
 	}
 }
 
@@ -28,27 +42,34 @@ list.addEventListener('click', function(ev) {
 
 // Create a new list item when clicking on the "ADD" button
 function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("task_input_field").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("New task cannot be empty.");
-  } else {
-    document.getElementById("task_list").appendChild(li);
-  }
-  document.getElementById("task_input_field").value = "";
+	var li = document.createElement("LI");
+	var inputValue = document.getElementById("task_input_field").value;
+	var t = document.createTextNode(inputValue);
+	li.appendChild(t);
+	if (inputValue === '') {
+		alert("New task cannot be empty.");
+	} else {
+		document.getElementById("task_list").appendChild(li);
+	}
+	document.getElementById("task_input_field").value = "";
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "remove_button";
-  span.appendChild(txt);
-  li.appendChild(span);
+	var removeSpan = document.createElement("SPAN");
+	var removeTxt = document.createTextNode("\u00D7");
+	removeSpan.className = "remove_button";
+	removeSpan.appendChild(removeTxt);
+	li.appendChild(removeSpan);
 
-  for (i = 0; i < removeButton.length; i++) {
-    removeButton[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
+	var editSpan = document.createElement("SPAN");
+	var editTxt = document.createTextNode("Edit");
+	editSpan.className = "edit_button";
+	editSpan.appendChild(editTxt);
+	li.appendChild(editSpan);
+
+	for (i = 0; i < removeButton.length; i++) {
+		removeButton[i].onclick = function() {
+			var div = this.parentElement;
+			console.log(div);
+			div.style.display = "none";
+		}
+	}
 }
